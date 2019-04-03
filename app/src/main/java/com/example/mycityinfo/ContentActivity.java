e9fa7c4ae05e1494153b252ba8472e27a8979b62
tamaldn <43597147+tamaldn@users.noreply.github.com>
2019-04-03 15:51:05 +0530

    ViewPager removed Imagae buttons added

--- app/src/main/java/com/example/mycityinfo/ContentActivity.java
+++ app/src/main/java/com/example/mycityinfo/ContentActivity.java
@@ -9,9 +9,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ContentActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageButton btn1,btn2,btn3;
+    private ImageButton btn1,btn2,btn3,btn4;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,22 @@ public class ContentActivity extends AppCompatActivity implements View.OnClickLi
         btn1 = findViewById(R.id.imageButton1);
         btn2 = findViewById(R.id.imageButton2);
         btn3 = findViewById(R.id.imageButton3);
+        btn4 = findViewById(R.id.imageButton4);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+    }
+
+    public void logout(){
+        firebaseAuth.signOut();
+        firebaseAuth.getCurrentUser().delete();
+        finish();
+        Intent i = new Intent(this,LoginActivity.class);
+        startActivity(i);
+        Toast.makeText(this,"Successfully Logged out",Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -43,6 +59,9 @@ public class ContentActivity extends AppCompatActivity implements View.OnClickLi
         {
             Intent i = new Intent(this,KathmanduActivity.class);
             startActivity(i);
+        }
+        if(v==btn4) {
+            logout();
         }
     }
 }
--- app/src/main/java/com/example/mycityinfo/DharmanagarActivity.java
+++ app/src/main/java/com/example/mycityinfo/DharmanagarActivity.java
@@ -2,12 +2,50 @@ package com.example.mycityinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class DharmanagarActivity extends AppCompatActivity {
+
+    private ExpandableListView expandableListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dharmanagar);
+
+        expandableListView = (ExpandableListView)findViewById(R.id.expandablelistview);
+        List<String> Headings = new ArrayList<String>();
+        List<String> L1 = new ArrayList<String>();
+        List<String> L2 = new ArrayList<String>();
+        List<String> L3 = new ArrayList<String>();
+        HashMap<String,List<String>> Childlist = new HashMap<String,List<String>>();
+        String heading_items[] = getResources().getStringArray(R.array.header_titles);
+        String l1[] = getResources().getStringArray(R.array.h1_items);
+        String l2[] = getResources().getStringArray(R.array.h2_items);
+        String l3[] = getResources().getStringArray(R.array.h3_items);
+
+        for(String title : heading_items)
+        {
+            Headings.add(title);
+        }
+        for(String title : l1)
+        {
+            L1.add(title);
+        }for(String title : l2)
+        {
+            L2.add(title);
+        }for(String title : l3)
+        {
+            L3.add(title);
+        }
+        Childlist.put(Headings.get(0),L1);
+        Childlist.put(Headings.get(1),L2);
+        Childlist.put(Headings.get(2),L3);
+        ListAdapter listAdapter = new ListAdapter(this, Headings, Childlist);
+        expandableListView.setAdapter(listAdapter);
     }
 }
