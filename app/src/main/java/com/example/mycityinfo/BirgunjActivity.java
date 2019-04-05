59f5b8b13d269661d67353427c6b6c051f8de28d
tamaldn <43597147+tamaldn@users.noreply.github.com>
2019-04-05 15:05:47 +0530

    Update string values

--- app/src/main/java/com/example/mycityinfo/BirgunjActivity.java
+++ app/src/main/java/com/example/mycityinfo/BirgunjActivity.java
@@ -9,26 +9,9 @@ import android.widget.TextView;
 
 public class BirgunjActivity extends AppCompatActivity {
 
-    Dialog myDialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.birgunj);
-
-        myDialog = new Dialog(this);
-    }
-
-    public  void showpopup(View v){
-        myDialog.setContentView(R.layout.custom_popup);
-        TextView textView;
-        textView = myDialog.findViewById(R.id.textView);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myDialog.dismiss();
-            }
-        });
-        myDialog.show();
     }
 }
